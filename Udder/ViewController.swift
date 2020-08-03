c8ad2b7b93da1d08b187111bac77caac67bcaf14
Victoria Boichenko <vik.boichenko@gmail.com>
2020-08-03 23:31:16 +0300

    Add assets and save other initial files

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Victoria Boichenko on 03.08.2020.
+//  Copyright © 2020 Victoria Boichenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
